91cd537c14463fdc28f6e26c33aa107ede00702b
Yashodhan Divakaran <yashodhan@Amples-MBP.localdomain>
2018-03-15 19:57:34 +0530

    json post and get request and response sample added in notes

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -3,4 +3,11 @@ module.exports = function(app, db) {
 		console.log(req.body);
 		resp.send('hello');
 	});
+	app.get('/notes', function (req, resp) {
+		resp.json({ error_code: 0, data: { msg: "Hello world" } } );
+	});
+	app.post('/notes/json', function (req, resp) {
+		console.log(req.json);
+		resp.json({ error_code: 0, data: { msg: "Hello world" } } );
+	});
 }
